9499e8f984ea48beeee70ecba382a65802480258
GoldenBurritoCode <o.b.mccomb@gmail.com>
2026-05-03 16:43:52 +0200

    v1.3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-window.alert("test")
-const json = document.getElementById("data").textContent;
-const data = json.parse(json)
+;
+
 let username;
 const title = document.getElementById("title")
 const submBtn = document.getElementById("mySubmit")
@@ -8,7 +7,6 @@ function clickedSubm() {
     username = document.getElementById("in1").value
     title.textContent = `Welkom ${username}!`
     submBtn.textContent = "ga naar rekenmachine"
-    document.getElementById("txt").style.display = data
     document.getElementById("usInLab").style.display = "none"
     document.getElementById("in1").style.display = "none"
     document.getElementById("undTitle").textContent = "Welkom op de site"
