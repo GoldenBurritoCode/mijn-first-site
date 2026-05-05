f6b2d7077f10faaf4a36f2adb0e784976ae7a2d0
GoldenBurrito <o.b.mccomb@gmail.com>
2026-05-05 19:40:56 +0200

    v2.51

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
-
 let username;
-const title = document.getElementById("title")
-const submBtn = document.getElementById("mySubmit")
+const title = document.getElementById("title");
+const submBtn = document.getElementById("mySubmit");
 function clickedSubm() {
     username = document.getElementById("in1").value
-    title.textContent = `Welkom ${username}!`
-    submBtn.textContent = "ga naar rekenmachine"
-    document.getElementById("usInLab").style.display = "none"
-    document.getElementById("in1").style.display = "none"
-    document.getElementById("undTitle").textContent = "Welkom op de site"
+    title.textContent = `Welkom ${username}!`;
+    submBtn.textContent = "ga naar rekenmachine";
+    document.getElementById("usInLab").style.display = "none";
+    document.getElementById("in1").style.display = "none";
+    document.getElementById("undTitle").textContent = "Welkom op de site";
 }
