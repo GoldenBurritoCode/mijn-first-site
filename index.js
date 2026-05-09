f8f9c4e885af2cb0a8aa763583d5ac1e1a54f956
GoldenBurrito <o.b.mccomb@gmail.com>
2026-05-09 11:18:59 +0200

    v1.2

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 let username;
+let times;
 const title = document.getElementById("title");
 const submBtn = document.getElementById("mySubmit");
 function clickedSubm() {
-    username = document.getElementById("in1").value;
-    title.textContent = `Welkom ${username}!`;
-    submBtn.textContent = "ga naar rekenmachine";
-    document.getElementById("usInLab").style.display = "none";
-    document.getElementById("in1").style.display = "none";
-    document.getElementById("undTitle").textContent = "Welkom op de site";
+    times ++;
+    if (times === 1) {
+        username = document.getElementById("in1").value;
+        title.textContent = `Welkom ${username}!`;
+        submBtn.textContent = "ga naar rekenmachine";
+        document.getElementById("usInLab").style.display = "none";
+        document.getElementById("in1").style.display = "none";
+        document.getElementById("undTitle").textContent = "Welkom op de site";
+    }
+    else if (times === 2) {
+        window.location.href = "calc/calc.html"
+    }
 }
+    
