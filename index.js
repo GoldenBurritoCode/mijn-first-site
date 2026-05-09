5d516c23b5bab2ebcbaef5d10b16975cd7311025
GoldenBurrito <o.b.mccomb@gmail.com>
2026-05-09 10:47:08 +0200

    just forgot 1 ;

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let username;
 const title = document.getElementById("title");
 const submBtn = document.getElementById("mySubmit");
 function clickedSubm() {
-    username = document.getElementById("in1").value
+    username = document.getElementById("in1").value;
     title.textContent = `Welkom ${username}!`;
     submBtn.textContent = "ga naar rekenmachine";
     document.getElementById("usInLab").style.display = "none";
